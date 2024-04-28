931e708ca13297cf3f754f3367029322cce7ab3d
SeiryuXD <162136822+SeiryuXD@users.noreply.github.com>
2024-04-28 20:19:11 +0700

    Update config.py
    Signed-off-by: SeiryuXD <162136822+SeiryuXD@users.noreply.github.com>

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -14,9 +14,9 @@ class Config(object):
     MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://te.legra.ph/file/40eb1ed850cdea274693e.jpg"
+    START_IMG = "https://telegra.ph/file/cb3d357856a02779161ca.jpg"
 
-    SUPPORT_CHAT = "DevilsHeavenMF"  # Your Telegram support group chat username where your users will go and bother you
+    SUPPORT_CHAT = "awoo"  # Your Telegram support group chat username where your users will go and bother you
 
     TOKEN = ""  # Get bot token from @BotFather on Telegram
 
